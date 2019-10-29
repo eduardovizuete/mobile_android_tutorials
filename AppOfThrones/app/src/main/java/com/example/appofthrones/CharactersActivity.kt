fcfc8a6337edd7149b66a7db6a212baa6f03a097
edu <vizuete.eduardo@gmail.com>
2019-10-29 14:16:11 -0500

    kotlin 0: 1.29 - 1.32 gravity align, logger, intents

--- AppOfThrones/app/src/main/java/com/example/appofthrones/CharactersActivity.kt
+++ AppOfThrones/app/src/main/java/com/example/appofthrones/CharactersActivity.kt
@@ -1,7 +1,24 @@
 package com.example.appofthrones
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CharactersActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_characters)
+
+        val button: Button = findViewById(R.id.button_character)
+        button.setOnClickListener {
+            Log.d("CharactersActivity", "Click is working")
+            val intent: Intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
 }
