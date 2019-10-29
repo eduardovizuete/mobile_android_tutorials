8d6750d5df6d84cbe63369d2f6f4d6e05ffe15e9
edu <vizuete.eduardo@gmail.com>
2019-10-29 12:41:43 -0500

    kotlin 0: 1.27 - 1.28 actividad crear, archivo manifest

---
+++ AppOfThrones/app/src/main/java/com/example/appofthrones/CharactersActivity.kt
@@ -0,0 +1,7 @@
+package com.example.appofthrones
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CharactersActivity: AppCompatActivity() {
+
+}
--- AppOfThrones/app/src/main/java/com/example/appofthrones/DetailActivity.kt
+++ AppOfThrones/app/src/main/java/com/example/appofthrones/DetailActivity.kt
@@ -1,12 +1,11 @@
 package com.example.appofthrones
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.button)
 
         button.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Winter is coming!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@DetailActivity, "Winter is coming!", Toast.LENGTH_SHORT).show()
         }
     }
 }
