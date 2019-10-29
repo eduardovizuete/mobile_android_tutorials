8f320b261bd1c59f1bea8a45dfc14b8c38063b3e
edu <vizuete.eduardo@gmail.com>
2019-10-29 11:06:45 -0500

    kotlin 0: 1.24 - Vista a controlador, click event

--- AppOfThrones/app/src/main/java/com/example/appofthrones/MainActivity.kt
+++ AppOfThrones/app/src/main/java/com/example/appofthrones/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.appofthrones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "Winter is coming!", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
-    
