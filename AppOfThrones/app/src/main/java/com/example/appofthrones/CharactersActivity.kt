0c2efecdfa7ccdb920614414566bf0ff9b8373e2
edu <vizuete.eduardo@gmail.com>
2019-10-29 14:36:09 -0500

    kotlin 0: 1.33 onClick listener desde xml

--- AppOfThrones/app/src/main/java/com/example/appofthrones/CharactersActivity.kt
+++ AppOfThrones/app/src/main/java/com/example/appofthrones/CharactersActivity.kt
@@ -3,22 +3,21 @@ package com.example.appofthrones
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-class CharactersActivity: AppCompatActivity() {
+class CharactersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_characters)
-
-        val button: Button = findViewById(R.id.button_character)
-        button.setOnClickListener {
-            Log.d("CharactersActivity", "Click is working")
-            val intent: Intent = Intent(this, DetailActivity::class.java)
-            startActivity(intent)
-        }
     }
 
+    fun showDetails(button: View) {
+        Log.d("CharactersActivity", "Click is working")
+        val intent: Intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
+    }
 }
+
