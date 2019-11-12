b67fbc6c73d0819078799cc0b607021c4e8134b8
edu <vizuete.eduardo@gmail.com>
2019-11-12 17:01:04 -0500

    kotlin 0: 4.8 Scroll View

--- AppOfThrones/app/src/main/java/com/example/appofthrones/DetailFragment.kt
+++ AppOfThrones/app/src/main/java/com/example/appofthrones/DetailFragment.kt
@@ -7,7 +7,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.data_character.*
+import kotlinx.android.synthetic.main.header_character.*
 
 class DetailFragment : Fragment() {
 
