549fd0f557bab64b3a643fdab9b52a558a2f35ab
edu <vizuete.eduardo@gmail.com>
2019-11-14 15:12:44 -0500

    kotlin 1: 3.5 sintaxis basica clases abstractas nuevo modulo

--- Gangame/app/src/main/java/com/example/gangame/deals/DealsFragment.kt
+++ Gangame/app/src/main/java/com/example/gangame/deals/DealsFragment.kt
@@ -1,21 +1,12 @@
 package com.example.gangame.deals
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.example.commons.BaseFragment
 import com.example.gangame.R
 
-class DealsFragment : Fragment() {
+class DealsFragment : BaseFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_deals, container, false)
-        return view
+    override fun getLayoutResId(): Int {
+        return R.layout.fragment_deals
     }
 
 }
