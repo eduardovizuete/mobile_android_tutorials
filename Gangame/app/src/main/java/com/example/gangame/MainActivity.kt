60d464309ada9a273db274324c4d0ded569bc9f7
edu <vizuete.eduardo@gmail.com>
2019-11-14 16:56:16 -0500

    kotlin 1: 3.8 - 3.12 Kotlin sintaxis extension
    3.8 - 3.9 Kotlin android extensions 
    3.10 - 3.12 Sintaxis basica val var setters getter e inferencia de tipos

--- Gangame/app/src/main/java/com/example/gangame/MainActivity.kt
+++ Gangame/app/src/main/java/com/example/gangame/MainActivity.kt
@@ -2,11 +2,62 @@ package com.example.gangame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.example.gangame.deals.DealsFragment
+import com.example.gangame.owned.TopOwnedFragment
+import com.example.gangame.rated.TopRatedFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val DEFAULT_OPTION = R.id.action_deals
+    }
+
+    val fragments: HashMap<Int, Fragment> = hashMapOf(
+        Pair(R.id.action_deals, DealsFragment()),
+        Pair(R.id.action_top_rated, TopRatedFragment()),
+        Pair(R.id.action_most_owned, TopOwnedFragment())
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+
+        navigationView.selectedItemId = DEFAULT_OPTION
+        navigationView.setOnNavigationItemSelectedListener { item ->
+            val fragment: Fragment? = fragments[item.itemId]
+
+            if (fragment != null) {
+                replaceFragment(fragment)
+            }
+
+            true
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+
+    // funcion inpura: altera la vista
+    fun initView() {
+        val currentFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainer)
+
+        if (currentFragment == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, fragments[DEFAULT_OPTION]!!)
+                .commit()
+        }
+    }
+
 }
