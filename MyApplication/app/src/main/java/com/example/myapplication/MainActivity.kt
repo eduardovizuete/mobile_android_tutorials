2d7c901ed66f271799d4934f50d5320faf8351aa
edu <vizuete.eduardo@gmail.com>
2019-11-26 08:39:33 -0500

    Variable exercise, string templates

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,12 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toast("Mensaje")
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = "Kotlin"
+        toast("Hello ${textView.text}, how are you")
     }
 
     fun toast(message: String): Unit {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
-    fun getInt(int: Int) = 2 * int
 }
