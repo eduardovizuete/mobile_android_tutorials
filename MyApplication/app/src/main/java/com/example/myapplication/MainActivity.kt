40a32b49f3e9591f717d8a020aa66e5df03251ad
edu <vizuete.eduardo@gmail.com>
2019-12-11 11:38:33 -0500

    Sealed classes

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,10 +3,8 @@ package com.example.myapplication
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
@@ -28,20 +26,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        MediaProvider.dataAsync { media ->
-            adapter.items = when (item.itemId) {
-                R.id.filter_all -> media
-                R.id.filter_photos -> media.filter { it.type == MediaItem.Type.PHOTO }
-                R.id.filter_videos -> media.filter { it.type == MediaItem.Type.VIDEO }
-                else -> emptyList()
-            }
+        val filter: Filter = when (item.itemId) {
+            R.id.filter_photos -> Filter.ByType(MediaItem.Type.PHOTO)
+            R.id.filter_videos -> Filter.ByType(MediaItem.Type.VIDEO)
+            else -> Filter.None
         }
 
+        loadFilteredData(filter)
+
         return true
+    }
+
+    private fun loadFilteredData(filter: Filter) {
+        MediaProvider.dataAsync { media ->
+            adapter.items = when (filter) {
+                Filter.None -> media
+                is Filter.ByType -> media.filter { it.type == filter.type }
+            }
+        }
     }
 
     private fun navigateToDetail(item: MediaItem) {
         startActivity<DetailActivity>(DetailActivity.ID to item.id)
     }
 
+}
+
+sealed class Filter {
+    object None : Filter()
+    class ByType(val type: MediaItem.Type) : Filter()
 }
