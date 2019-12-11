e62c57b30ea4e7087d2dd5636fa841169f6df1a1
edu <vizuete.eduardo@gmail.com>
2019-12-11 15:16:38 -0500

    Couroutines, context and dispatchers

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,10 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
@@ -26,24 +30,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val filter: Filter = when (item.itemId) {
+        val filter = when (item.itemId) {
+            R.id.filter_all -> Filter.None
             R.id.filter_photos -> Filter.ByType(MediaItem.Type.PHOTO)
             R.id.filter_videos -> Filter.ByType(MediaItem.Type.VIDEO)
-            else -> Filter.None
+            else -> null
         }
 
-        loadFilteredData(filter)
+        filter?.let {
+            GlobalScope.launch(Dispatchers.Main) {
+                val media1 = withContext(Dispatchers.IO) { MediaProvider.dataSync("cats") }
+                // if the sentence above is too long, is posible to extract to suspend function, for instance getData
+                val media2 = getData("nature")
+                updateData(media1 + media2, filter)
+            }
 
-        return true
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
-    private fun loadFilteredData(filter: Filter) {
-        MediaProvider.dataAsync { media ->
-            adapter.items = when (filter) {
-                Filter.None -> media
-                is Filter.ByType -> media.filter { it.type == filter.type }
-            }
+    // it is posible extract to suspend function
+    private suspend fun getData(type: String): List<MediaItem> = withContext(Dispatchers.IO) {
+        MediaProvider.dataSync(type)
+    }
+
+    private fun updateData(media: List<MediaItem>, filter: Filter = Filter.None) {
+        adapter.items = when (filter) {
+            Filter.None -> media
+            is Filter.ByType -> media.filter { it.type == filter.type }
         }
+
     }
 
     private fun navigateToDetail(item: MediaItem) {
--- MyApplication/app/src/main/java/com/example/myapplication/MediaProvider.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MediaProvider.kt
@@ -2,29 +2,32 @@ package com.example.myapplication
 
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import kotlin.random.Random
 
 object MediaProvider {
 
-    private val thumbBase = "http://lorempixel.com/400/400/cats/"
+    private const val thumbBase = "http://lorempixel.com/400/400/"
+    private val rnd = Random(1)
 
     private var data = emptyList<MediaItem>()
 
-    fun dataAsync(callback: (List<MediaItem>) -> Unit) {
-        doAsync {
-            if (data.isEmpty()) {
-                Thread.sleep(2000)
-                data = (1..10).map {
-                    MediaItem(
-                        it,
-                        "Title $it",
-                        "${thumbBase}$it",
-                        if (it % 3 == 0) MediaItem.Type.VIDEO else MediaItem.Type.PHOTO
-                    )
-                }
-            }
-            uiThread {
-                callback(data)
-            }
+    private fun randomType() =
+        rnd.nextInt(2).let { if (it == 0) MediaItem.Type.PHOTO else MediaItem.Type.VIDEO }
+
+    fun dataAsync(type: String = "cats", f: (List<MediaItem>) -> Unit) = doAsync {
+        if (data.isEmpty()) {
+            data = dataSync(type)
+        }
+
+        uiThread {
+            f(data)
+        }
+    }
+
+    fun dataSync(type: String): List<MediaItem> {
+        Thread.sleep(5000)
+        return (1..10).map {
+            MediaItem(it, "Title $it", "${thumbBase}$type/$it", randomType())
         }
     }
 
