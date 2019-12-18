c9ec49b0f96a9c2fb2188218af2ec8d367d1c012
edu <vizuete.eduardo@gmail.com>
2019-12-18 17:25:42 -0500

    Extract media provider as an interface and inject it through constructor

--- MyApplication/app/src/main/java/com/example/myapplication/MediaProvider.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MediaProvider.kt
@@ -4,7 +4,11 @@ import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import kotlin.random.Random
 
-object MediaProvider {
+interface Provider {
+    fun dataAsync(f: (List<MediaItem>) -> Unit)
+}
+
+object MediaProvider : Provider {
     private const val thumbBase = "http://lorempixel.com/400/400"
     private val rnd = Random(1)
 
@@ -13,17 +17,19 @@ object MediaProvider {
     private fun randomType() =
         rnd.nextInt(2).let { if (it == 0) MediaItem.Type.PHOTO else MediaItem.Type.VIDEO }
 
-    fun dataAsync(f: (List<MediaItem>) -> Unit) = doAsync {
-        if (data.isEmpty()) {
-            data = dataSync("cats")
-        }
+    override fun dataAsync(f: (List<MediaItem>) -> Unit) {
+        doAsync {
+            if (data.isEmpty()) {
+                data = dataSync("cats")
+            }
 
-        uiThread {
-            f(data)
+            uiThread {
+                f(data)
+            }
         }
     }
 
-    fun dataSync(dataType: String): List<MediaItem> {
+    private fun dataSync(dataType: String): List<MediaItem> {
         Thread.sleep(5000)
         return (1L..10L).map {
             MediaItem(
